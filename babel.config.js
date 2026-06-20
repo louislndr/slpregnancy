86b1f71e77afbb69d0b4533ec3bb79bc2032504a
louislndr <landreaulouis1@icloud.com>
2026-06-20 14:06:43 -0400

    fix: add expo-linking, correct babel-preset-expo version, add placeholder assets
    - Install expo-linking (required by expo-router internals)
    - Downgrade babel-preset-expo to ~54.0.10 to match SDK 54
    - Remove deprecated loose option from Babel plugins
    - Add placeholder PNG assets for icon/splash/favicon
    
    Co-Authored-By: louislndr <landreaulouis1@icloud.com>

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-proposal-class-properties',
     ],
   };
 };
